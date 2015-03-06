b66da004b5cb5a050d0804051ef238ab1fde5fb9
Oskar Sigvardsson <oskarsigvardsson@gmail.com>
2015-03-06 18:09:57 +0100

    added addition chain challenge description

--- hard/addition-chains/achain.pl
+++ hard/addition-chains/achain.pl
@@ -1,64 +1,60 @@
 :- use_module(library(clpfd)).
 
-find_chain(Length, Target, Chain) :-
-    L is Length - 3,
-    length(Vars, L),
-    append([[1,2], Vars, [Target]], Chain),
-    [1,2|Rest] = Chain,
-    star_chain([2,1], Rest, Target, 2, Length).
 
 find_chain(Length, Target, Chain) :-
-    write("No star chain found, checking other chains...\n"),
-    L is Length - 3,
+    L is Length - 2,
     length(Vars, L),
     append([[1,2], Vars, [Target]], Chain),
     [1,2|Rest] = Chain,
     bind_vars([2,1], Rest, Target, 2, Length).
 
-star_chain(Prev, [Target], Target, _, _) :-
-    [P|_] = Prev,
-    member(A, Prev),
-    Target =:= A + P.
-
-star_chain(Prev, [Curr|Rest], Target, Index, Length) :-
-    Rest \= [],
-    [P|_] = Prev,
-    star_step(Prev, Curr),
-    Curr > P,
-    Curr =< Target,
-    Target =< Curr ** (Length - Index + 1),
-
-    Index2 is Index + 1,
-
-    star_chain([Curr|Prev], Rest, Target, Index2, Length).
 
 bind_vars(Prev, [Target], Target, _, _) :-
+    !,
     [P|_] = Prev,
     member(A, Prev),
     Target =:= A + P.
 
 bind_vars(Prev, [Curr|Rest], Target, Index, Length) :- 
     Rest \= [],
-    [P1, P2|_] = Prev,
-    (P1 =:= P2 << 1 ->
-        star_step(Prev, Curr);
-        regular_step(Prev, Curr) 
-    ),
+    [P1|_] = Prev,
+    member(A, Prev),
+    member(B, Prev),
+    B =< A,
+    Curr is A + B,
+    ub(Curr, Ub),
+    Index =< Ub,
     Curr > P1,    
     Curr =< Target,
+    
     Target =< Curr ** (Length - Index + 1),
     
     Index2 is Index + 1,
 
     bind_vars([Curr|Prev], Rest, Target, Index2, Length).
 
-regular_step(Prev, Curr) :- star_step(Prev, Curr).
-regular_step(Prev, Curr) :- non_star_step(Prev, Curr).
+ub(N, M) :- 
+    lambda(N, M1),
+    nu(N, M2),
+    M is M1 + M2 - 1.
 
-star_step([P|Prev], Curr) :-
-    member(A, [P|Prev]),
-    Curr is P + A.
+lambda(N, M) :- lambda(N, 0, M).
 
-non_star_step([_|Prev], Curr) :-
-    member(A, Prev), member(B, Prev),
-    Curr is A + B.
+lambda(1, X, X) :- !.
+lambda(N, Acc, M) :-
+    N > 1, !,
+    N1 is N >> 1,
+    Acc1 is Acc + 1,
+    lambda(N1, Acc1, M).
+
+nu(N, M) :- nu(N, 1, M).
+nu(1, X, X) :- !.
+nu(N, Acc, M) :- 
+    N > 1, !,
+    N1 is N >> 1,
+    (
+    N /\ 1 =:= 0 ->
+        Acc1 is Acc;
+        Acc1 is Acc + 1
+    ),
+    nu(N1, Acc1, M).
