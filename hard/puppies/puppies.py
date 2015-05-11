36dcdbfe7cdaa3ad3d1e12f87a185f46d1f31599
Oskar Sigvardsson <oskarsigvardsson@gmail.com>
2015-05-11 13:23:59 +0200

    added rövarspråket and stdev

--- hard/puppies/puppies.py
+++ hard/puppies/puppies.py
@@ -1,33 +1,41 @@
-from itertools import permutations
-from random import choice
-from collections import Counter
-
 NEG_INF = float("-inf")
 
+# I should probably use one of those fancy enums for this
+ANY = 0
+HIGHER = 1
+EQUAL = 2
+LOWER = 3
+
+# Memoization dictionary
 memo = {}
 
+# Is <treat> higher/lower/equal to <to>
 def relation_pass(treat, relation, to):
-    if not relation:
+    if relation == ANY:
         return True
-    if relation == "higher":
+    if relation == HIGHER:
         return treat > to
-    if relation == "lower":
+    if relation == LOWER:
         return treat < to
-    if relation == "equal":
+    if relation == EQUAL:
         return treat == to
 
-def happiness(treats, relation = None, to = None):
+# Lets calculate optimal happiness!
+def happiness(treats, relation = ANY, to = None):
 
+    # Recursion base case
     if len(treats) == 1:
         if not relation_pass(treats[0], relation, to):
             return NEG_INF, tuple()
-        if relation == "higher":
+        if relation == HIGHER:
             return 1, treats
-        if relation == "lower":
+        if relation == LOWER:
             return -1, treats
-        if relation == "equal":
+        if relation == EQUAL:
             return 0, treats
 
+    # Check if we've already seen this subproblem. If we have, return that,
+    # otherwise proceed
     try: 
         return memo[(treats, relation, to)]
     except KeyError:
@@ -37,87 +45,62 @@ def happiness(treats, relation = None, to = None):
     
     tried_treats = set()
 
+    # Loop over the treats
     for t, treat in enumerate(treats):
-        if treat in tried_treats:
-            continue
+
+        # Is the current treat valid for our relation?
         if not relation_pass(treat, relation, to):
             continue
+
+        # If we've already tried this treat, skip this round. Doesn't change
+        # the asymptotic running time, but saves a few function calls and
+        # hash-lookups. 
+        if treat in tried_treats:
+            continue
         
-        if relation == None:
-            print(t)
         tried_treats.add(treat)
 
+        # New treat list for recursion. If the input treats are sorted, this
+        # will be as well, which is crucial for the memoization dictionary
         new_treats = treats[:t] + treats[t+1:]
         
-        top1, order1 = happiness(new_treats, "higher", treat)
-        top2, order2 = happiness(new_treats, "equal", treat)
-        top3, order3 = happiness(new_treats, "lower", treat)
+        # Recurse!
+        top1, order1 = happiness(new_treats, HIGHER, treat)
+        top2, order2 = happiness(new_treats, EQUAL, treat)
+        top3, order3 = happiness(new_treats, LOWER, treat)
         
         order1 = (treat,) + order1
         order2 = (treat,) + order2
         order3 = (treat,) + order3
 
-        if not relation or relation == "lower":
+        # Modify score based on what the relation is for this call and the
+        # recursion
+        if relation == ANY or relation == LOWER:
             top1 -= 1
-        if not relation or relation == "higher":
+        if relation == ANY or relation == HIGHER:
             top3 += 1
 
-
+        # Which is our winner?
         top, order = max(
             (top, order),
             (top1, order1), 
             (top2, order2), 
             (top3, order3))
 
+    # Save calculated subproblem
     memo[(treats, relation, to)] = (top, order)
 
     return top, order
 
-def puppy_score(n, treats):
-    if n == 0:
-        if treats[0] == treats[1]:
-            return 0
-        return -1 if treats[0] < treats[1] else 1
-    if n == len(treats) - 1:
-        if treats[-1] == treats[-2]:
-            return 0
-        return -1 if treats[-1] < treats[-2] else 1
-
-    p1, p2, p3 = treats[n-1], treats[n], treats[n+1]
-
-    if p1 > p2 and p3 > p2:
-        return -1
-    if p1 < p2 and p3 < p2:
-        return 1
-
-    return 0
-
-def simple_happiness(treats):
-    puppy_count = len(treats)
-
-    top, order = NEG_INF, tuple()
-    
-    for perm in permutations(treats):
-        score = sum(puppy_score(n, perm) for n in range(puppy_count))
-        
-        top, order = max((top,order), (score, perm))
-
-    return top, order
-
 if __name__ == "__main__":
-    target = 1
-    happy = 0
-    treats = None
-    order = None
+    # The algorithm relies on the inputs being a tuple of sorted integers, so
+    # lets just make sure
+    treats = tuple(sorted(map(int, 
+        "1 1 2 2 2 2 2 2 3 4 4 4 5 5 5 6 6 6 7 7 8 8 9 9 9 9 9 9 9 9".split())))
 
-    while happy < target:
-        puppies = 30
-        space = list(range(1, 13))
-
-        treats = tuple(sorted(choice(space) for _ in range(puppies)))
-        print(treats)
-        happy, order = happiness(treats)
-
-        print(happy, order)
-
-
+    # Calculate!
+    happiness, order = happiness(treats)
+    
+    # Print!
+    print happiness
+    print " ".join(str(x) for x in order)
